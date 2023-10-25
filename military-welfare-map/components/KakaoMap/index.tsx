e3acbd94e2eb6290b5dd95df5045725f0f29c5dd
Daniel Oh <ohdoyoel@naver.com>
2023-10-25 23:00:06 +0900

    [feat] building map,  버튼 누르면 현재 위치로 가는거 짜다가 막힘

--- military-welfare-map/components/KakaoMap/index.tsx
+++ military-welfare-map/components/KakaoMap/index.tsx
@@ -1,18 +1,93 @@
 "use client"
 
-import { Map } from 'react-kakao-maps-sdk'
+import PreviousMap from 'postcss/lib/previous-map'
+import { useEffect, useState } from 'react'
+import { Map, MapTypeControl, ZoomControl, MapMarker } from 'react-kakao-maps-sdk'
 
-export const KakaoMap = () => {    
-    return (
-        <Map center={{
-                lat: 33.450701,
-                lng: 126.570667,
-            }}
-            style={{
-                width: "100%",
-                height: "100%",
-            }}
-            level={3}
-        />
+export const KakaoMap = () => {
+
+    // get current position and mark
+
+    const [currentPositionState, setCurrentPositionState] = useState({
+        center: {
+            lat: 33.450701,
+            lng: 126.570667,
+        },
+        errMsg: "",
+        isLoading: true,
+    })
+    
+    useEffect(() => {
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(
+                (position) => {
+                    setCurrentPositionState((prev) => ({
+                        ...prev,
+                        center: {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude,
+                        },
+                        isLoading: false,
+                    }))
+                    setLocationState((prev) => ({
+                        center: {
+                            lat: position.coords.latitude,
+                            lng: position.coords.longitude,
+                        }
+                    }))
+                },
+                (err) => {
+                    setCurrentPositionState((prev) => ({
+                        ...prev,
+                        errMsg: err.message,
+                        isLoading: false,
+                    }))
+                }
+                )
+            }
+        }, [])
+        
+        // move map function
+        
+        const [locationState, setLocationState] = useState({
+            center: {
+                lat: currentPositionState.center.lat,
+                lng: currentPositionState.center.lng,
+            },
+            isPanTo: false,
+        })
+        
+        // const panTo = (lat:number, lng:number) => {
+        //     setLocationState({
+        //         center: {
+        //             lat: lat,
+        //             lng: lng,
+        //         },
+        //         isPanTo: true
+        //     })
+        // }
+        
+        return (
+        <div className='relative w-full h-full'>        
+            <Map center={locationState.center}
+                isPanto={locationState.isPanTo}
+                style={{
+                    width: "100%",
+                    height: "100%",
+                }}
+                level={10}>
+                {!currentPositionState.isLoading &&
+                    <MapMarker position={currentPositionState.center}
+                        image={{
+                            src: "/current-position.png",
+                            size: {width: 20, height: 20},
+                            options: {offset: {x: 0, y: 0}},
+                        }}
+                    />}
+                <MapTypeControl position={"TOPRIGHT"} />
+                <ZoomControl position={"RIGHT"} />
+            </Map>
+            {/* <span className="material-icons w-10 h-10 absolute bottom-0 right-0 z-10" onClick={panTo(currentPositionState.center.lat, currentPositionState.center.lng)}/> */}
+        </div>
     )
 }
