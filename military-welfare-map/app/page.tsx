3bf0decc07142c87ca50b96a2a72cc080c2a512d
Daniel Oh <ohdoyoel@gmail.com>
2023-11-11 05:14:14 +0000

    [feat] panel

--- military-welfare-map/app/page.tsx
+++ military-welfare-map/app/page.tsx
@@ -1,21 +1,35 @@
 "use client"
 
 import Image from 'next/image'
+import { Transition } from '@headlessui/react'
 import { KakaoMap } from '@/components/KakaoMap'
 import { Header } from '@/components/Header'
 import { useState } from 'react'
 
 export default function Home() {
-  const [isBarShowing, setIsBarShowing] = useState(true)
+  const [isBarOpened, setIsBarOpened] = useState(false)
   
   return (
     <main className="flex flex-row w-screen h-screen">
-      <div className={`flex flex-col 
-                      transition ease-in-out duration-150 ${isBarShowing ? `w-1/3` : `w-auto`} `}>
+      <div className={`${isBarOpened ? `block` : `hidden`} basis-1/2 h-full bg-white z-10 h-full shadow-[2px_2px_2px_0_rgba(0,0,0,0.3)]`} >
         <Header/>
-        <button className='w-4 h-4' onClick={() => {setIsBarShowing(!isBarShowing)}}/>
+        {/* info */}
       </div>
-      <KakaoMap isBarShowing={isBarShowing}/>
+
+      <div className={`absolute inset-y-0 w-auto z-20
+                      ${isBarOpened ? `left-1/3` : `left-0`} flex items-center 
+                      `}>
+        <button className='w-6 h-12 bg-white rounded-r-[3px] shadow-[2px_2px_2px_0_rgba(0,0,0,0.3)]' onClick={() => {setIsBarOpened(!isBarOpened)}}>
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 25 25" fill="none" transform={`${isBarOpened ? `rotate(180)` : `rotate(0)`}`}>
+            <path d="M10.5 8L15 12.5L10.5 17" stroke="#3396ff" strokeWidth="3"/>
+          </svg>
+        </button>
+      </div>
+
+      <div className='basis-full h-full z-0'>
+        <KakaoMap/>
+      </div>
+
     </main>
   )
 }
--- military-welfare-map/components/Header/index.tsx
+++ military-welfare-map/components/Header/index.tsx
@@ -1,6 +1,10 @@
 export const Header = () => {
         return (
-        <div className='w-full h-20 bg-[#3396ff]'>
+        <div className='w-full h-16 bg-[#3396ff] flex items-center'>
+            <a href="" className="flex flex-row pl-4">
+                <p className="text-xl text-white font-thin">병영생활</p>
+                <p className="text-xl text-white font-bold">지도</p>
+            </a>
         </div>
     )
 }
