6a040a5c63b6d879483e49b0f31363458b2f303a
Daniel Oh <ohdoyoel@gmail.com>
2023-11-12 14:56:38 +0000

    [feat] toggle buttons bar using grid

--- military-welfare-map/src/components/ToggleTags/index.tsx
+++ military-welfare-map/src/components/ToggleTags/index.tsx
@@ -10,6 +10,7 @@ import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
 import LocalHospitalOutlinedIcon from '@mui/icons-material/LocalHospitalOutlined';
 import MilitaryTechOutlinedIcon from '@mui/icons-material/MilitaryTechOutlined';
 import GolfCourseOutlinedIcon from '@mui/icons-material/GolfCourseOutlined';
+import { useState } from 'react';
 
 interface ToggleTagsProps {
 }
@@ -20,61 +21,71 @@ interface TagToggleButton {
 }
 
 export const ToggleTags = () => {
-        return (
+    // const [r, setR] = useState(true)
+    // const [c, setC] = useState(true)
+    // const [h, setH] = useState(true)
+    // const [b, setB] = useState(true)
+    // const [a, setA] = useState(true)
+    // const [h, setH] = useState(true)
+    // const [s, setS] = useState(true)
+    // const [t, setT] = useState(true)
+    // const [g, setG] = useState(true)
+    // const [h, setH] = useState(true)
+    // const [m, setM] = useState(true)
+    // const [g, setG] = useState(true)
+
+    return (
         <div className="w-full h-16 bg-[#3396ff] px-4 flex items-center">
-            <div className='w-full h-full flex flex-row justify-between flex-nowrap overflow-auto'>
-            <button className='flex flex-col w-24 items-center text-xs text-white'>
-                <RestaurantOutlinedIcon className='text-lg'/>
-                음식점
-            </button>
-            <button className='flex flex-col w-24 items-center text-xs text-white'>
-                <CoffeeOutlinedIcon className='text-lg'/>
-                카페
-            </button>
-            <button className='flex flex-col w-24 items-center text-xs text-white'>
-                <ContentCutOutlinedIcon className='text-lg'/>
-                미용실
-            </button>
-            <button className='flex flex-col w-24 items-center text-xs text-white'>
-                <HotTubOutlinedIcon className='text-lg'/>
-                목욕탕
-            </button>
-            <button className='flex flex-col w-24 items-center text-xs text-white'>
-                <AttractionsOutlinedIcon className='text-lg'/>
-                문화·여가
-            </button>
-            <button className='flex flex-col w-24 items-center text-xs text-white'>
-                <HotelOutlinedIcon className='text-lg'/>
-                숙박
-            </button>
-            <button className='flex flex-col w-24 items-center text-xs text-white'>
-                <SportsSoccerOutlinedIcon className='text-lg'/>
-                스포츠
-            </button>
-            <button className='flex flex-col w-24 items-center text-xs text-white'>
-                <TrainOutlinedIcon className='text-lg'/>
-                교통·항공
-            </button>
-            <button className='flex flex-col w-24 items-center text-xs text-white'>
-                <VisibilityOutlinedIcon className='text-lg'/>
-                안경점
-            </button>
-            <button className='flex flex-col w-24 items-center text-xs text-white'>
-                <LocalHospitalOutlinedIcon className='text-lg'/>
-                병원
-            </button>
-            <button className='flex flex-col w-24 items-center text-xs text-white'>
-                <MilitaryTechOutlinedIcon className='text-lg'/>
-                예비군
-            </button>
-            <button className='flex flex-col w-24 items-center text-xs text-white'>
-                <GolfCourseOutlinedIcon className='text-lg'/>
-                골프장
-            </button>
-
+            <div className='w-full h-14 grid grid-cols-12 gap-12 content-center overflow-x-auto'>
+                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
+                    <RestaurantOutlinedIcon className='text-lg'/>
+                    음식점
+                </button>
+                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
+                    <CoffeeOutlinedIcon className='text-lg'/>
+                    카페
+                </button>
+                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
+                    <ContentCutOutlinedIcon className='text-lg'/>
+                    미용실
+                </button>
+                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
+                    <HotTubOutlinedIcon className='text-lg'/>
+                    목욕탕
+                </button>
+                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
+                    <AttractionsOutlinedIcon className='text-lg'/>
+                    문화·여가
+                </button>
+                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
+                    <HotelOutlinedIcon className='text-lg'/>
+                    숙박
+                </button>
+                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
+                    <SportsSoccerOutlinedIcon className='text-lg'/>
+                    스포츠
+                </button>
+                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
+                    <TrainOutlinedIcon className='text-lg'/>
+                    교통·항공
+                </button>
+                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
+                    <VisibilityOutlinedIcon className='text-lg'/>
+                    안경점
+                </button>
+                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
+                    <LocalHospitalOutlinedIcon className='text-lg'/>
+                    병원
+                </button>
+                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
+                    <MilitaryTechOutlinedIcon className='text-lg'/>
+                    예비군
+                </button>
+                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
+                    <GolfCourseOutlinedIcon className='text-lg'/>
+                    골프장
+                </button>
             </div>
-            
-
         </div>
     )
 }
