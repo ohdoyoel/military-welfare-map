13b25b7984a18dca87d687abf9434b774dc938cf
Daniel Oh <ohdoyoel@gmail.com>
2023-11-15 14:52:15 +0000

    [design] design improvement

--- military-welfare-map/src/components/Header/index.tsx
+++ military-welfare-map/src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
         return (
-        <div className='w-full h-14 bg-[#3396ff]'>
+        <div className='w-full h-14 bg-emerald-500'>
             <a href="" className="flex flex-row p-4">
                 <p className="text-xl text-white font-nsr">병영생활</p>
                 <p className="text-xl text-white font-nsb">지도</p>
--- military-welfare-map/src/components/ToggleTags/index.tsx
+++ military-welfare-map/src/components/ToggleTags/index.tsx
@@ -10,81 +10,178 @@ import VisibilityOutlinedIcon from '@mui/icons-material/VisibilityOutlined';
 import LocalHospitalOutlinedIcon from '@mui/icons-material/LocalHospitalOutlined';
 import MilitaryTechOutlinedIcon from '@mui/icons-material/MilitaryTechOutlined';
 import GolfCourseOutlinedIcon from '@mui/icons-material/GolfCourseOutlined';
-import { useState } from 'react';
+import React, { ReactElement, useState } from 'react';
+import { ToggleTagButton } from '../ToggleTagButton';
 
 interface ToggleTagsProps {
 }
 
-interface TagToggleButton {
-    icon: string,
+interface ToggleTagButtonProps {
+    icon: ReactElement<any, any>,
     label: string
 }
 
 export const ToggleTags = () => {
-    // const [r, setR] = useState(true)
-    // const [c, setC] = useState(true)
-    // const [h, setH] = useState(true)
-    // const [b, setB] = useState(true)
-    // const [a, setA] = useState(true)
-    // const [h, setH] = useState(true)
-    // const [s, setS] = useState(true)
-    // const [t, setT] = useState(true)
-    // const [g, setG] = useState(true)
-    // const [h, setH] = useState(true)
-    // const [m, setM] = useState(true)
-    // const [g, setG] = useState(true)
+    const [isToggled01, setToggleButton01] = useState(false)
+    const [isToggled02, setToggleButton02] = useState(false)
+    const [isToggled03, setToggleButton03] = useState(false)
+    const [isToggled04, setToggleButton04] = useState(false)
+    const [isToggled05, setToggleButton05] = useState(false)
+    const [isToggled06, setToggleButton06] = useState(false)
+    const [isToggled07, setToggleButton07] = useState(false)
+    const [isToggled08, setToggleButton08] = useState(false)
+    const [isToggled09, setToggleButton09] = useState(false)
+    const [isToggled10, setToggleButton10] = useState(false)
+    const [isToggled11, setToggleButton11] = useState(false)
+    const [isToggled12, setToggleButton12] = useState(false)
+
+    const onToggleButtonClicked = (btnIdx: Number) => {
+        switch(btnIdx) { 
+            case 1: { 
+                setToggleButton01(!isToggled01)
+                break; 
+            } case 2: { 
+                setToggleButton02(!isToggled02)
+                break; 
+            } case 3: {
+                setToggleButton03(!isToggled03)
+                break;
+            } case 4: { 
+                setToggleButton04(!isToggled04)
+                break; 
+            } case 5: { 
+                setToggleButton05(!isToggled05)
+                break; 
+            } case 6: { 
+                setToggleButton06(!isToggled06)
+                break; 
+            } case 7: { 
+                setToggleButton07(!isToggled07)
+                break; 
+            } case 8: { 
+                setToggleButton08(!isToggled08)
+                break; 
+            } case 9: { 
+                setToggleButton09(!isToggled09)
+                break; 
+            } case 10: { 
+                setToggleButton10(!isToggled10)
+                break; 
+            } case 11: { 
+                setToggleButton11(!isToggled11)
+                break; 
+            } case 12: { 
+                setToggleButton12(!isToggled12)
+                break; 
+            } default: { 
+                console.log("btnIdx error")
+                break; 
+            } 
+        } 
+    }
+
+    const isToggled = (btnIdx: Number) => {
+        switch(btnIdx) { 
+            case 1: { 
+                return isToggled01
+            } case 2: { 
+                return isToggled02
+            } case 3: {
+                return isToggled03
+            } case 4: { 
+                return isToggled04
+            } case 5: { 
+                return isToggled05
+            } case 6: { 
+                return isToggled06
+            } case 7: { 
+                return isToggled07
+            } case 8: { 
+                return isToggled08
+            } case 9: { 
+                return isToggled09
+            } case 10: { 
+                return isToggled10
+            } case 11: { 
+                return isToggled11
+            } case 12: { 
+                return isToggled12
+            } default: { 
+                console.log("btnIdx error")
+                return false
+            } 
+        } 
+    }
+
+    const toggleTagButtonList = () => {
+        const iconAndLabelData: ToggleTagButtonProps[] = [
+            {
+                icon: <RestaurantOutlinedIcon className='text-lg text-white'/>,
+                label: '음식점'
+            },
+            {
+                icon: <CoffeeOutlinedIcon className='text-lg text-white'/>,
+                label: '카페'
+            },
+            {
+                icon: <ContentCutOutlinedIcon className='text-lg text-white'/>,
+                label: '미용실'
+            },
+            {
+                icon: <HotTubOutlinedIcon className='text-lg text-white'/>,
+                label: '목욕탕'
+            },
+            {
+                icon: <AttractionsOutlinedIcon className='text-lg text-white'/>,
+                label: '문화·여가'
+            },
+            {
+                icon: <HotelOutlinedIcon className='text-lg text-white'/>,
+                label: '숙박'
+            },
+            {
+                icon: <SportsSoccerOutlinedIcon className='text-lg text-white'/>,
+                label: '스포츠'
+            },
+            {
+                icon: <TrainOutlinedIcon className='text-lg text-white'/>,
+                label: '교통·항공'
+            },
+            {
+                icon: <VisibilityOutlinedIcon className='text-lg text-white'/>,
+                label: '안경점'
+            },
+            {
+                icon: <LocalHospitalOutlinedIcon className='text-lg text-white'/>,
+                label: '병원'
+            },
+            {
+                icon: <MilitaryTechOutlinedIcon className='text-lg text-white'/>,
+                label: '예비군'
+            },
+            {
+                icon: <GolfCourseOutlinedIcon className='text-lg text-white'/>,
+                label: '골프장'
+            },
+        ]
+
+        const result = []
+        for (let i = 0; i < iconAndLabelData.length; i++) {
+            result.push(
+                <ToggleTagButton onClicked={() => onToggleButtonClicked(i+1)} isToggled={isToggled(i+1)} label={iconAndLabelData[i].label} key={i}>
+                    {iconAndLabelData[i].icon}
+                </ToggleTagButton>
+            )
+        }
+
+        return result
+    }
+
 
     return (
-        <div className="w-full h-16 bg-[#3396ff] px-4 flex items-center">
-            <div className='w-full h-14 grid grid-cols-12 gap-12 content-center overflow-x-auto'>
-                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
-                    <RestaurantOutlinedIcon className='text-lg'/>
-                    음식점
-                </button>
-                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
-                    <CoffeeOutlinedIcon className='text-lg'/>
-                    카페
-                </button>
-                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
-                    <ContentCutOutlinedIcon className='text-lg'/>
-                    미용실
-                </button>
-                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
-                    <HotTubOutlinedIcon className='text-lg'/>
-                    목욕탕
-                </button>
-                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
-                    <AttractionsOutlinedIcon className='text-lg'/>
-                    문화·여가
-                </button>
-                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
-                    <HotelOutlinedIcon className='text-lg'/>
-                    숙박
-                </button>
-                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
-                    <SportsSoccerOutlinedIcon className='text-lg'/>
-                    스포츠
-                </button>
-                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
-                    <TrainOutlinedIcon className='text-lg'/>
-                    교통·항공
-                </button>
-                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
-                    <VisibilityOutlinedIcon className='text-lg'/>
-                    안경점
-                </button>
-                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
-                    <LocalHospitalOutlinedIcon className='text-lg'/>
-                    병원
-                </button>
-                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
-                    <MilitaryTechOutlinedIcon className='text-lg'/>
-                    예비군
-                </button>
-                <button className='flex flex-col w-[50px] items-center text-xs text-white rounded-[3px] hover:bg-black'>
-                    <GolfCourseOutlinedIcon className='text-lg'/>
-                    골프장
-                </button>
+        <div className="w-full h-16 bg-emerald-500 px-4 flex items-center">
+            <div className='w-full h-14 grid grid-cols-12 gap-14 content-center overflow-x-auto'>
+                {toggleTagButtonList()}
             </div>
         </div>
     )
