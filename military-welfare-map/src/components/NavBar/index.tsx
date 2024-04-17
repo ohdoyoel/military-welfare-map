b6eedfbc13395babd158fcd27d7bbcd7f456ea5d
Daniel Oh <ohdoyoel@gmail.com>
2024-04-17 12:19:54 +0000

    navButton

--- military-welfare-map/src/components/NavBar/index.tsx
+++ military-welfare-map/src/components/NavBar/index.tsx
@@ -1,12 +1,22 @@
+import { NavButton } from "../NavButton"
+import SearchIcon from '@mui/icons-material/Search';
+import ChatIcon from '@mui/icons-material/Chat';
+
 export const NavBar = () => {
-        return (
-        <div className='w-full h-14 bg-emerald-500 flex flex-row justify-around items-center'>
-            <button className="w-24 h-10 bg-emerald-600">
-                <p className="text-base text-white">장소 검색</p>
-            </button>
-            <button className="w-24 h-10 bg-emerald-600">
-                <p className="text-base text-white">오상병 부르기</p>
-            </button>
+    const isClicked = () => {
+        console.log("hello")
+    }
+
+    return (
+        <div className='w-full h-20 p-2 bg-emerald-500 flex flex-row justify-around items-center'>
+            <NavButton isToggled={true} onClicked={isClicked}>
+                <SearchIcon className='text-white' fontSize='medium'/>
+                <p className="text-base text-white font-nsr">장소 검색</p>
+            </NavButton>
+            <NavButton isToggled={false} onClicked={isClicked}>
+                <ChatIcon className='text-white' fontSize='medium'/>
+                <p className="text-base text-white font-nsr">오상병 부르기</p>
+            </NavButton>
         </div>
     )
 }
