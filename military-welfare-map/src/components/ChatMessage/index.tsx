bd0e88852b18de8980cb47f0d087a7f57ee030a2
Daniel Oh <ohdoyoel@gmail.com>
2024-04-19 15:05:10 +0000

    not move to End

--- military-welfare-map/src/components/ChatMessage/index.tsx
+++ military-welfare-map/src/components/ChatMessage/index.tsx
@@ -1,9 +1,12 @@
+import { LegacyRef } from "react"
+
 interface ChatMessageProps {
+    ref?: LegacyRef<HTMLDivElement>
     message: string
     isBotSide: boolean
 }
 
-export const ChatMessage = ({message, isBotSide}: ChatMessageProps) => {
+export const ChatMessage = ({ref, message, isBotSide}: ChatMessageProps) => {
     
     const nowTime = () : string => {
         const date = new Date()
@@ -11,10 +14,12 @@ export const ChatMessage = ({message, isBotSide}: ChatMessageProps) => {
     }
     
     return (
-        <div className={`flex items-start gap-2.5 ${isBotSide ? "self-start" : "self-end mt-4"}`}>
+        <div className={`flex items-start gap-2.5 ${isBotSide ? "self-start" : "self-end mt-4"}`} ref={ref}>
             {!isBotSide ? <span className="text-sm font-normal text-gray-500 self-end">{nowTime()}</span> : ""}
             <div className="flex flex-col gap-1 w-full max-w-[320px]">
-                {isBotSide ? <span className="text-sm font-semibold text-gray-900">오상병</span> : ""}
+                <span className={`text-sm font-semibold text-gray-900 ${!isBotSide ? "text-end" : ""}`}>
+                    {isBotSide ? "오상병" : "나"}
+                </span>
                 <div className={`flex flex-col leading-1.5 p-4 bg-white ${isBotSide ? "rounded-r-lg rounded-bl-lg" : "rounded-l-lg rounded-br-lg"} shadow-[2px_2px_2px_0_rgba(0,0,0,0.3)] border-l-2`}>
                     <p className="text-sm font-normal text-gray-900">{message}</p>
                 </div>
---
+++ military-welfare-map/src/functions/useMoveScroll.ts
@@ -0,0 +1,9 @@
+import { useRef } from 'react';
+
+export const useMoveScroll = () => {
+    const element = useRef<HTMLDivElement>(null);
+    const onMoveToElement = () => {
+        element.current?.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    };
+    return {element, onMoveToElement};
+}
