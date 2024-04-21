163d9c911fc2be7b5f48834258633697d00335e9
Daniel Oh <ohdoyoel@gmail.com>
2024-04-21 15:12:58 +0000

    header design added

---
+++ military-welfare-map/src/components/Header2/index.tsx
@@ -0,0 +1,10 @@
+export const Header2 = () => {
+    return (
+        <div className='h-10 bg-emerald-500 rounded-lg m-2 p-2 shadow-[2px_2px_2px_0_rgba(0,0,0,0.3)]'>
+            <a href="" className="flex flex-row">
+                <p className="text-xl text-white font-nsr">병영생활</p>
+                <p className="text-xl text-white font-nsb">지도</p>
+            </a>
+        </div>
+    )
+}
