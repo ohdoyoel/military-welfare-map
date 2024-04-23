3e063968dca08d13aa31675b0a703234f0496686
Daniel Oh <ohdoyoel@gmail.com>
2024-04-23 14:54:43 +0000

    chat panel development

--- military-welfare-map/src/components/ChatMessage/index.tsx
+++ military-welfare-map/src/components/ChatMessage/index.tsx
@@ -13,16 +13,16 @@ export const ChatMessage = ({message, isBotSide}: ChatMessageProps) => {
     
     return (
         <div className={`flex items-start gap-2.5 ${isBotSide ? "self-start" : "self-end mt-4"}`}>
-            {!isBotSide ? <p className="text-sm font-normal text-gray-500 self-end">{nowTime()}</p> : ""}
+            {!isBotSide ? <p className="text-base font-normal text-gray-500 self-end">{nowTime()}</p> : ""}
             <div className="flex flex-col gap-1 w-full max-w-[320px]">
-                <p className={`text-sm font-semibold text-gray-900 ${!isBotSide ? "text-end" : ""}`}>
-                    {isBotSide ? "오상병" : "나"}
+                <p className={`text-lg font-semibold text-white ${!isBotSide ? "text-end" : ""}`}>
+                    {isBotSide ? "병영생활지G도PT" : "나"}
                 </p>
                 <div className={`flex flex-col leading-1.5 p-4 bg-white ${isBotSide ? "rounded-r-lg rounded-bl-lg" : "rounded-l-lg rounded-br-lg"} shadow-[2px_2px_2px_0_rgba(0,0,0,0.3)] border-l-2 text-sm font-normal text-gray`}>
-                    <p className="text-sm font-normal text-gray-900">{message}</p>
+                    <p className="text-lg font-normal">{message}</p>
                 </div>
             </div>
-            {isBotSide ? <p className="text-sm font-normal text-gray-500 self-end">{nowTime()}</p> : ""}
+            {isBotSide ? <p className="text-base font-normal text-gray-500 self-end">{nowTime()}</p> : ""}
         </div>
     )
 }
