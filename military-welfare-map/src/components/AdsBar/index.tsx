b1e478e22964ee088855b4ff849ace84d1b1f518
Daniel Oh <ohdoyoel@gmail.com>
2024-04-23 12:24:09 +0000

    adsBar

--- military-welfare-map/src/components/AdsBar/index.tsx
+++ military-welfare-map/src/components/AdsBar/index.tsx
@@ -5,7 +5,7 @@ export const AdsBar = ({} : AdsBarProps) => {
     return (
         <div className='w-full h-32 p-2 bg-slate-300'>
             <a href="" className="w-full h-full flex items-center justify-center">
-                <p className="text-3xl font-nse text-slate-100">This Is Ads</p>
+                <p className="text-3xl font-nse text-slate-100">This Is Advertising</p>
             </a>
         </div>
     )
