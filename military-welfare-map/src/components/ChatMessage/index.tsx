6b993da4cc17fec94ead6b2a3f42854bb67a6c45
Daniel Oh <ohdoyoel@gmail.com>
2024-04-24 14:45:00 +0000

    chat design

--- military-welfare-map/src/components/ChatMessage/index.tsx
+++ military-welfare-map/src/components/ChatMessage/index.tsx
@@ -7,22 +7,23 @@ export const ChatMessage = ({message, isBotSide}: ChatMessageProps) => {
     
     const nowTime = () : string => {
         const date = new Date()
-        // return date.getHours() + ":" + date.getMinutes()
-        return ""
+        return ("0" + date.getHours()).slice(-2) + ":" + ("0" + date.getMinutes()).slice(-2)
     }
     
     return (
-        <div className={`flex items-start gap-2.5 ${isBotSide ? "self-start" : "self-end mt-4"}`}>
-            {!isBotSide ? <p className="text-base font-normal text-gray-500 self-end">{nowTime()}</p> : ""}
-            <div className="flex flex-col gap-1 w-full max-w-[320px]">
-                <p className={`text-lg font-semibold text-white ${!isBotSide ? "text-end" : ""}`}>
+        <div className="flex flex-col w-full mt-2">
+            <p className={`text-lg font-semibold text-white ${!isBotSide ? "text-end" : ""}`}>
                     {isBotSide ? "병영생활지G도PT" : "나"}
-                </p>
-                <div className={`flex flex-col leading-1.5 p-4 bg-white ${isBotSide ? "rounded-r-lg rounded-bl-lg" : "rounded-l-lg rounded-br-lg"} shadow-[2px_2px_2px_0_rgba(0,0,0,0.3)] border-l-2 text-sm font-normal text-gray`}>
-                    <p className="text-lg font-normal">{message}</p>
+            </p>
+            <div className={`flex items-start gap-2.5 ${isBotSide ? "self-start flex-row-reverse" : "self-end"}`}>
+                <p className="text-base font-normal text-white self-end" suppressHydrationWarning>{nowTime()}</p>
+                <div className="flex flex-col gap-1 w-fit max-w-[320px]">
+                    <div className={`flex flex-col p-2 bg-white ${isBotSide ? "rounded-r-lg rounded-bl-lg" : "rounded-l-lg rounded-br-lg"} shadow-[2px_2px_2px_0_rgba(0,0,0,0.3)] border-l-2`}>
+                        <p className="text-lg font-normal">{message}</p>
+                    </div>
                 </div>
             </div>
-            {isBotSide ? <p className="text-base font-normal text-gray-500 self-end">{nowTime()}</p> : ""}
         </div>
+
     )
 }
