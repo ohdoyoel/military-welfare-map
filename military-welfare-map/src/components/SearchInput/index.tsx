76207957019d7e4f3ba8e5f9fcf5d91271c2214c
Daniel Oh <ohdoyoel@gmail.com>
2024-04-27 07:50:59 +0000

    btn redesign and ui improvement

--- military-welfare-map/src/components/SearchInput/index.tsx
+++ military-welfare-map/src/components/SearchInput/index.tsx
@@ -7,11 +7,11 @@ interface SearchInputProps {
 export const SearchInput = ({onKeyUp}: SearchInputProps) => {
 
     return (
-        <div className="w-full h-12 bg-emerald-500 flex justify-center px-4">
-            <div className='relative w-full shadow-[2px_2px_2px_0_rgba(0,0,0,0.3)]'>
-                <SearchIcon className='absolute top-2 left-2.5 text-gray-500' fontSize='medium'/>
+        <div className="flex-none w-full h-12 bg-emerald-500 flex justify-center px-4">
+            <div className='relative w-full h-full shadow-[2px_2px_2px_0_rgba(0,0,0,0.3)]'>
+                <SearchIcon className='absolute top-[12px] left-2.5 text-gray-500' fontSize='medium'/>
                 <input id="searchInput" type="text" onKeyUp={onKeyUp}
-                        className="h-full w-full p-3 pl-10 text-base text-gray-900 rounded-[3px] focus:outline-none" placeholder="상호명, 주소(지역명), 종류, 전화번호로 검색보세요">
+                        className="h-full w-full p-3 pl-10 text-base text-gray-900 rounded-[3px] focus:outline-none" placeholder="상호명, 주소(지역명), 태그, 전화번호로 검색해보십시오">
                 </input>
             </div>
         </div>
--- military-welfare-map/src/components/ToggleRegionButton2/index.tsx
+++ military-welfare-map/src/components/ToggleRegionButton2/index.tsx
@@ -9,7 +9,7 @@ export const ToggleRegionButton2 = ({isToggled, onClicked, label}: ToggleRegionB
         <button className={`flex flex-col w-16 items-center rounded-[3px] shadow-[2px_2px_2px_0_rgba(0,0,0,0.3)]
                             ${isToggled
                                 ? `shadow-[inset_2px_2px_2px_0_rgba(0,0,0,0.3)] bg-emerald-500 text-white `
-                                : `shadow-[2px_2px_2px_0_rgba(0,0,0,0.3)] bg-white`
+                                : `shadow-[2px_2px_2px_0_rgba(0,0,0,0.3)] bg-white text-gray-600`
                             }
                             `}
                 onClick={onClicked}>
