4fba9111a876752ecdcbb5814bced1bad2bb2248
Daniel Oh <ohdoyoel@gmail.com>
2024-05-03 15:31:26 +0000

    floating ads

--- military-welfare-map/src/components/AdsWindow/index.tsx
+++ military-welfare-map/src/components/AdsWindow/index.tsx
@@ -1,7 +1,7 @@
 import { ReactElement } from "react"
 import { tagOrderBgColor, tagOrderBgGradientColor } from "@/src/types/tagColor";
 import { tagIconForInfoWindow, tagLabel, tagToOrder } from "@/src/types/tagIconLabel";
-import { MapInfoWindow, MapMarker } from "react-kakao-maps-sdk";
+import { CustomOverlayMap, MapInfoWindow, MapMarker } from "react-kakao-maps-sdk";
 
 interface AdsWindowProps {
     idx: number
@@ -11,18 +11,18 @@ interface AdsWindowProps {
 const idxToMargin = [
     '-ml-80 -mt-40',
     '-ml-80 -mt-4',
-    '-ml-80 mt-40',
+    '-ml-80 mt-52',
 ]
 
 // 부모의 부모의 설정값을 바꿔야 함
 export const AdsWindow = ({idx, pos}: AdsWindowProps) => {
     return (
-        <MapMarker position={pos}>
+        <CustomOverlayMap position={pos}>
             <div className={`w-0 h-0  ${idxToMargin[idx]}`}>
                 <a href="" className="w-80 h-32 bg-slate-200 shadow-[2px_2px_2px_0_rgba(0,0,0,0.3)] rounded-[3px] flex items-center justify-center">
                     <p className="text-4xl font-nse text-slate-100">This Is Advertising</p>
                 </a>
             </div>
-        </MapMarker>
+        </CustomOverlayMap>
     )
 }
