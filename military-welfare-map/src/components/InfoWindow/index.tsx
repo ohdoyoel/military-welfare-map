c4a48a1fb820fa334cd61584ac61ed9fc62fb63e
Daniel Oh <ohdoyoel@gmail.com>
2024-05-04 15:02:45 +0000

    infowidow star 위치

--- military-welfare-map/src/components/InfoWindow/index.tsx
+++ military-welfare-map/src/components/InfoWindow/index.tsx
@@ -1,10 +1,9 @@
 import { Dispatch, ReactElement, SetStateAction, useEffect, useRef } from "react"
-import { tagOrderBgColor, tagOrderBgGradientColor } from "@/src/types/tagColor";
+import { tagOrderBgColor, tagOrderBgGradientColor, tagOrderTextColor } from "@/src/types/tagColor";
 import { tagIconForInfoWindow, tagLabel, tagToOrder } from "@/src/types/tagIconLabel";
 import { MarkerType } from "@/src/types/data";
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import { HdrOffSelect } from "@mui/icons-material";
 
 interface InfoWindowProps {
     pos: {lat:number, lng:number}
@@ -42,10 +41,7 @@ export const InfoWindow = ({pos, tag, address, title, description, telno, onFire
         <div className={`relative flex flex-col absolute -left-1/2 ${onFire ? `ml-8` : `ml-6`} bottom-[236px] z-20`}>
             <div className="flex flex-row h-48 bg-white rounded-[3px] shadow-[2px_2px_2px_0_rgba(0,0,0,0.3)]">
                 <div className={`flex-none w-1 h-full ${tagOrderBgColor[tagToOrder[tag]].dark} rounded-l-[3px]`}/>
-                <div className={`relative flex-none w-20 h-full ${onFire ? tagOrderBgGradientColor[tagToOrder[tag]]: tagOrderBgColor[tagToOrder[tag]].normal} flex flex-col items-center justify-center text-white`}>
-                    <button className={`absolute top-1 left-1`} onClick={() => starToggle(title)}>
-                    {star ? <FavoriteIcon className='text-lg'/> : <FavoriteBorderIcon className='text-lg'/>}
-                    </button>
+                <div className={`flex-none w-20 h-full ${onFire ? tagOrderBgGradientColor[tagToOrder[tag]]: tagOrderBgColor[tagToOrder[tag]].normal} flex flex-col items-center justify-center text-white`}>
                     {tagIconForInfoWindow[tag]}
                     <p className='text-sm'>{tagLabel[tag]}</p>
                 </div>
@@ -53,8 +49,11 @@ export const InfoWindow = ({pos, tag, address, title, description, telno, onFire
                     <p className='text-left text-lg font-nsb'>{title}</p>
                     <p className='text-left text-base mr-4'>{address}</p>
                     <p className='pt-1 text-left text-sm'>{telno}</p>
-                    <p className='pt-2 text-left text-xs whitespace-pre-wrap'>{imgSrcRef.current ? description?.substring(description.lastIndexOf(')')+1) : description}</p>
-                    <a className={`absolute bottom-2 grid justify-center right-2 w-20 h-8 ${onFire ? tagOrderBgGradientColor[tagToOrder[tag]]: tagOrderBgColor[tagToOrder[tag]].normal} rounded-[3px] place-content-center`}
+                    <p className='pt-2 text-left text-xs whitespace-pre-wrap'>{imgSrcRef.current ? description?.substring(description.lastIndexOf(')')+1) : description}</p>                    
+                    <button className={`absolute bottom-2 right-24 w-8 h-8 grid place-content-center`} onClick={() => starToggle(title)}>
+                        {star ? <FavoriteIcon className={`text-lg ${tagOrderTextColor[tagToOrder[tag]].normal}`}/> : <FavoriteBorderIcon className={`text-lg ${tagOrderTextColor[tagToOrder[tag]].normal}`}/>}
+                    </button>
+                    <a className={`absolute bottom-2 right-2 grid justify-center w-20 h-8 ${onFire ? tagOrderBgGradientColor[tagToOrder[tag]]: tagOrderBgColor[tagToOrder[tag]].normal} rounded-[3px] place-content-center`}
                         href={`https://map.kakao.com/link/to/${title},${pos.lat},${pos.lng}`} target='_blank'>
                         <p className="after:content-['_↗'] text-center text-sm align-middle text-white text-pretty">길찾기</p>
                     </a>
