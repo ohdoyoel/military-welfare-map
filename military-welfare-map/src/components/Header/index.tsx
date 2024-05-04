fef8544c5db361a169421c8cf2451616429cd677
Daniel Oh <ohdoyoel@gmail.com>
2024-05-04 14:34:01 +0000

    minor design modification

--- military-welfare-map/src/components/Header/index.tsx
+++ military-welfare-map/src/components/Header/index.tsx
@@ -14,13 +14,14 @@ export const Header = ({isStarToggled, setIsStarToggled}: HeaderProps) => {
                 <p className="text-2xl text-white font-nsb">병영생활</p>
                 <p className="text-2xl text-white font-nse">지도</p>
             </a>
-            <button className={`flex flex-row gap-2 px-1 items-center h-8 z-10 rounded-[3px] m-2 focus:outline-none text-white text-2xl
+            <button className={`flex flex-row gap-2 px-1 items-center h-8 z-10 rounded-[3px] m-2 focus:outline-none text-white
                                 ${isStarToggled
                                     ? `shadow-[inset_2px_2px_2px_0_rgba(0,0,0,0.3)] bg-emerald-600`
                                     : `shadow-[2px_2px_2px_0_rgba(0,0,0,0.3)]`} 
-                                `} onClick={() => setIsStarToggled(!isStarToggled)}>
-                <p className="text-sm text-white font-nsr">찜 장소만 표시</p>
-                {isStarToggled ? <FavoriteIcon/> : <FavoriteBorder/>}
+                                `}
+                                onClick={() => setIsStarToggled(!isStarToggled)}>
+                <p className="text-xs text-white font-nsr">찜한 장소</p>
+                {isStarToggled ? <FavoriteIcon className="text-base"/> : <FavoriteBorder className="text-base"/>}
             </button>
         </div>
     )
