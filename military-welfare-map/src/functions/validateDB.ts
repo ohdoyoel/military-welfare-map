85653738270c68f5fc960ae3c41545459da65fd5
Daniel Oh <ohdoyoel@gmail.com>
2024-05-05 08:29:08 +0000

    db duplicated deletion

---
+++ military-welfare-map/src/functions/validateDB.ts
@@ -0,0 +1,11 @@
+import { MarkerType } from "../types/data";
+
+export const validateDB = (markers :MarkerType[]) => {
+    console.log('validation start')
+    for (let i=0; i<markers.length-1; i++) {
+        for (let j=i+1; j<markers.length; j++) {
+            if (markers[i].title == markers[j].title && Math.abs(markers[i].position.lat-markers[j].position.lat) < 0.0005) console.log(markers[i].title)
+        }
+    }
+    console.log('validation end')
+}
