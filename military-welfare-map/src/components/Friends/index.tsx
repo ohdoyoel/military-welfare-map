ecc3830bc29abb4ba27bfa681804bf9e911e40fc
seosubalbyeong moon <jscsumin3579@uos.ac.kr>
2024-05-13 14:25:28 +0000

    friends update

--- military-welfare-map/src/components/Friends/index.tsx
+++ military-welfare-map/src/components/Friends/index.tsx
@@ -8,6 +8,7 @@ interface FriendsProps {
 export const Friends = ({setIsFriendsOpened}: FriendsProps) => {
     const [dyProfileOpened, setDyProfileOpened] = useState(false)
     const [smProfileOpened, setSmProfileOpened] = useState(false)
+    const [usedDataOpened, setUsedDataOpened] = useState(false)
 
     return (
         <div className="absolute inset-x-0 inset-y-0 flex flex-col w-full bg-slate-50 shadow-[2px_2px_2px_0_rgba(0,0,0,0.3)] z-20 overflow-y-auto">
@@ -20,7 +21,7 @@ export const Friends = ({setIsFriendsOpened}: FriendsProps) => {
             </div> */}
             
             <div className="flex flex-col pb-2 border-b-2 border-slate-200 ">
-                <p className="flex-none text-slate-400 mx-4 mt-2">챗봇</p>
+                <p className="flex-none text-slate-400 mx-4">챗봇</p>
                 <div className="flex-none flex flex-row items-center w-full my-2 cursor-pointer hover:bg-emerald-100"
                     onClick={() => setIsFriendsOpened(false)}>
                     <div className="w-16 h-16 mx-4 my-2">
@@ -34,7 +35,7 @@ export const Friends = ({setIsFriendsOpened}: FriendsProps) => {
             </div>
 
             <div className="flex flex-col pb-2 border-b-2 border-slate-200 ">
-                <p className="flex-none text-slate-400 mx-4 mt-2">지피티 병장의 후임들</p>
+                <p className="flex-none text-slate-400 mx-4 my-2">지피티 병장의 후임들</p>
                 <div className="flex-none flex flex-row items-center w-full py-2">
                     <div className="w-12 h-12 mx-4">
                         <img className="rounded-[20px] border-[1px] border-slate-300" src="/images/sumin.jfif" alt="/images/sumin.jfif"/>
@@ -56,7 +57,7 @@ export const Friends = ({setIsFriendsOpened}: FriendsProps) => {
             </div>
 
             <div className="flex flex-col pb-2 border-b-2 border-slate-200 ">
-                <p className="flex-none text-slate-400 mx-4 mt-2">도움주신 분들</p>
+                <p className="flex-none text-slate-400 mx-4 my-2">도움주신 분들</p>
                 <a className="flex-none flex flex-row items-center w-full py-2 hover:bg-slate-100" href={'https://www.mnd.go.kr/mbshome/mbs/mnd/index.jsp'} target="_blank">
                     <div className="w-12 h-12 mx-4 rounded-[20px] border-[1px] border-slate-300 ">
                         <img className="p-1.5" src="/images/mnd.png" alt="/images/mnd.png" />
@@ -87,7 +88,7 @@ export const Friends = ({setIsFriendsOpened}: FriendsProps) => {
             </div>
 
             <div className="flex flex-col pb-2 border-b-2 border-slate-200 ">
-                <p className="flex-none text-slate-400 mx-4 mt-2">활용 기술</p>
+                <p className="flex-none text-slate-400 mx-4 my-2">활용 기술</p>
                 <a className="flex-none flex flex-row items-center w-full py-2 hover:bg-slate-100" href="https://nextjs.org/" target="_blank">
                     <div className="w-12 h-12 mx-4 rounded-[20px] border-[1px] border-slate-300 grid place-content-center">
                         <img src="/images/nextjs.png" alt="/images/nextjs.png"/>
@@ -106,7 +107,7 @@ export const Friends = ({setIsFriendsOpened}: FriendsProps) => {
                         <span className="text-sm mx-1 text-slate-400">CSS</span>
                     </div>
                 </a>
-                <a className="flex-none flex flex-row items-center w-full py-2 hover:bg-slate-100" href="https://apis.map.kakao.com/">
+                <a className="flex-none flex flex-row items-center w-full py-2 hover:bg-slate-100" href="https://apis.map.kakao.com/" target="_blank">
                     <div className="w-12 h-12 mx-4 rounded-[20px] border-[1px] bg-slate-200 border-slate-300 grid place-content-center">
                         <img width={'30px'} height={'30px'} src="/images/kakaomap.png" alt="/images/kakaomap.png"/>
                     </div>
@@ -115,7 +116,7 @@ export const Friends = ({setIsFriendsOpened}: FriendsProps) => {
                         <span className="text-sm mx-1 text-slate-400">Map API</span>
                     </div>
                 </a>
-                <a className="flex-none flex flex-row items-center w-full py-2 hover:bg-slate-100" href="https://www.netlify.com/">
+                <a className="flex-none flex flex-row items-center w-full py-2 hover:bg-slate-100" href="https://www.netlify.com/" target="_blank">
                     <div className="w-12 h-12 mx-4 grid place-content-center">
                         <img className="rounded-[20px] border-[1px] bg-slate-200 border-slate-300" src="/images/netlify.webp" alt="/images/netlify.webp"/>
                     </div>
@@ -126,31 +127,21 @@ export const Friends = ({setIsFriendsOpened}: FriendsProps) => {
                 </a>
             </div>
 
-            <div className="flex flex-col pb-2 border-b-2 border-slate-200 ">
-                <p className="flex-none text-slate-400 mx-4 mt-2">활용공공데이터</p>
-                {usedData.map(({title, imgSrc, desc, link}) => (
+            <div className="flex flex-col pb-2 border-b-2 border-slate-200">
+                <p className="flex-none text-slate-400 mx-4 my-2 cursor-pointer" onClick={() => setUsedDataOpened(!usedDataOpened)}>
+                    활용공공데이터 {usedDataOpened ? '▼' : '◄'}
+                </p>
+                {usedDataOpened && usedData.map(({title, imgSrc, desc, link}) => (
                     <a className="flex-none flex flex-row items-center w-full py-2 hover:bg-slate-100" href={link} target="_blank">
                         <div className="flex-none w-8 h-8 mx-4 rounded-[12px] border-[1px] border-slate-300 ">
                             <img className="p-1.5" src={imgSrc} alt={imgSrc} />
                         </div>
                         <div className="flex flex-col">
-                            <span className="text-base mx-1">{title}</span>
-                            <span className="text-xs mx-1 text-slate-400">{desc}</span>
+                            <span className="text-base mx-1 pr-4">{title}</span>
+                            <span className="text-xs mx-1 text-slate-400 pr-4">{desc}</span>
                         </div>
                     </a>
                 ))}
-                {/* <div className="flex-none flex flex-row items-center w-full py-2">
-                    <div className="w-12 h-12 bg-emerald-500 mx-4 rounded-[20px]"/>
-                    <span className="text-lg m-1">대한민국 국방부</span>
-                </div>
-                <div className="flex-none flex flex-row items-center w-full py-2">
-                    <div className="w-12 h-12 bg-emerald-500 mx-4 rounded-[20px]"/>
-                    <span className="text-lg m-1">병무청</span>
-                </div>
-                <div className="flex-none flex flex-row items-center w-full py-2">
-                    <div className="w-12 h-12 bg-emerald-500 mx-4 rounded-[20px]"/>
-                    <span className="text-lg m-1">방위사업청</span>
-                </div> */}
             </div>
         </div>
     )
