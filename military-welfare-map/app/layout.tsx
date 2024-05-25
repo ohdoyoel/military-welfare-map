18e94a33877ba7ab127b60ecf7618443f6e6912a
seosubalbyeong moon <jscsumin3579@uos.ac.kr>
2024-05-25 01:48:08 +0000

    naver verification

--- military-welfare-map/app/layout.tsx
+++ military-welfare-map/app/layout.tsx
@@ -4,10 +4,29 @@ import Script from 'next/script'
 
 export const metadata: Metadata = {
   title: '병영생활지도',
-  description: '',
+  description: '병영 생활을 도와주는 지도, 병영생활지도',
   icons: {
 		icon: "/icon.png",
 	},
+  applicationName: '병영생활지도',
+  authors: {
+    url: 'https://github.com/ohdoyoel',
+    name: 'ohdoyoel'
+  },
+  generator: 'Next.js',
+  keywords: ['병영생활지도', '국방공공데이터활용경진대회'],
+  creator: 'ohdoyoel',
+  publisher: 'netlify',
+  verification: {
+    other: {
+    'naver-site-verification': '6297d1ac3e9a48116f889c68bcbc9fd066591f24',
+    }
+  },
+  openGraph: {
+    title: '병영생활지도',
+    description: '병영 생활을 도와주는 지도, 병영생활지도',
+    images: '/icon.png',
+  },
 }
 
 export default function RootLayout({
