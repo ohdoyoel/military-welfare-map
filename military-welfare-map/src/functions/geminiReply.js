d71179c4cc89575e76f95e881894bfbf123f7f90
Daniel Oh <ohdoyoel@gmail.com>
2024-07-01 14:57:38 +0000

    gemini

--- military-welfare-map/src/functions/geminiReply.js
+++ military-welfare-map/src/functions/geminiReply.js
@@ -10,8 +10,28 @@ const {
 const apiKey = process.env.NEXT_PUBLIC_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
+const safetySetting = [
+  {
+    category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+    threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+    threshold: HarmBlockThreshold.BLOCK_ONLY_HIGH,
+  },
+  {
+    category: HarmCategory.HARM_CATEGORY_SEXUALLY_EXPLICIT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+},
+{
+    category: HarmCategory.HARM_CATEGORY_DANGEROUS_CONTENT,
+    threshold: HarmBlockThreshold.BLOCK_NONE,
+},
+];
+
 const model = genAI.getGenerativeModel({
   model: "gemini-1.5-flash",
+  safetySettings: safetySetting
 });
 
 const generationConfig = {
